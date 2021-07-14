edd57fea61a6744340b0e206ed509b5c18c39631
Turner Bell <turner@tbell.us>
2021-07-14 09:08:32 -0400

    initial commit

---
+++ src/app/hero.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+import { Observable, of } from 'rxjs';
+import { HEROES } from './mock-heroes';
+import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+	getHeroes(): Observable<Hero[]> {
+		const heroes = of(HEROES);
+		this.messageService.add('HeroService: fetched heroes');
+		return heroes;
+	}
+
+	getHero(id: number): Observable<Hero> {
+		const hero = HEROES.find(h => h.id === id)!;
+		this.messageService.add(`HeroService: fetched hero id=${id}`);
+		return of(hero);
+	}
+
+  constructor(private messageService: MessageService) { }
+}
